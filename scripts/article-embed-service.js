5e417d3e639681b472d075edd337d04137b653ce
Vaminta <gregory.card@hotmail.co.uk>
2024-05-24 23:56:20 +0100

    added article metadata icons

--- scripts/article-embed-service.js
+++ scripts/article-embed-service.js
@@ -1,18 +1,42 @@
 // article embed service
 
-function postMetadata(){
+let article = new Object();
+article.metadata = {
+	title: "Article",
+	author: "Unknown",
+	published: null,
+	updated: null
+};
+
+article.metadata.generate = function(){
 	const titleText = document.getElementsByTagName("title")[0].innerHTML;
+	article.metadata.title = titleText;
+	const mdContainer = document.getElementById("meta-data-container");
+	if(!mdContainer) return;
+	
+	const authorText = document.getElementById("author-span").innerHTML;
+	if(authorText.length>1) article.metadata.author = authorText;
+	
+	const published = document.getElementById("published-span").innerHTML;
+};
+
+function postMetadata(){
 	let data = {
 		sender: "content-page",
 		type: "meta-data",
 		value: {
-			title: titleText
+			title: article.metadata.title
 		}
 	};
 	window.parent.postMessage(data,"*");
 }
 
+function receiveMessage(event){
+	let data = event.data;
+}
+
 function initialise(){
+	article.metadata.generate();
 	postMetadata();
 }
 
@@ -22,3 +46,5 @@ var interval = setInterval(function(){
 		initialise();
 	}
 },200);
+
+window.addEventListener("message", receiveMessage, false);
